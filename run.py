cea5785d725b6bf9e3d71cd32b2731e2768e02ad
Jordi Vazquez <jordivazqueza@gmail.com>
2014-10-04 16:51:32 +0100

    Jordi

--- run.py
+++ run.py
@@ -15,12 +15,17 @@ def hello_monkey():
  
     #NewPart
     body_message = str(request.values.get('Body', None))
-    #rocket_name = body_message.split(' ') [0]
-    #motor = body_message.split(' ') [1]
+    rocket_name = body_message.split(' ') [0]
+    motor = body_message.split(' ') [1]
 
     from_number = request.values.get('From', None)
+  '''
     if from_number in callers:
         message = callers[from_number] + ", thanks for the message!"
+
+   '''
+   if from_number in callers:
+        message = "Hi " + callers[from_number] + "! Rocket name: " + rocket_name + " Motor: " + motor
     else:
         message = "Monkey, thanks for the message!"
  
