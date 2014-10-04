3fcce9439aeeef0b53e0f13ec5a2e6e22a488bbd
Jordi Vazquez <jordivazqueza@gmail.com>
2014-10-04 16:43:25 +0100

    Jordi

--- run2.py
+++ run2.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 # Try adding your own number to this list!
 callers = {
     "+447763501564": "Jordi",
-    "+447804654075": "Thomas",
+    "+14158675310": "Bootsso",
+    "+14158675311": "Virgil",
 }
  
 @app.route("/", methods=['GET', 'POST'])
@@ -14,10 +15,19 @@ def hello_monkey():
     """Respond and greet the caller by name."""
  
     from_number = request.values.get('From', None)
+
+    #NewPart
+    body_message = request.values.get('Body', None)
+    rocket_name = body_message.split(' ') [0]
+    motor = body_message.split(' ') [1]
+'''
+    if from_number in callers:
+        message = "Hi " + callers[from_number] + "! Rocket name: " + rocket_name + " Motor: " + motor
+'''
     if from_number in callers:
         message = callers[from_number] + ", thanks for the message!"
     else:
-        message = "Monkey, thanks for the message!"
+        message = "Phone not found"
  
     resp = twilio.twiml.Response()
     resp.message(message)
